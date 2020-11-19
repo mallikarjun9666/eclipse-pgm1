2e0954f39855ce9075c7c6fd8d0aef6e8b372ec7
arjun <arjun123@gmail.com>
2020-11-19 16:35:23 +0530

    commit javapgm 1

---
+++ src/test/java/javapgm1/multiplicationtabletest.java
@@ -0,0 +1,14 @@
+package javapgm1;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class multiplicationtabletest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
